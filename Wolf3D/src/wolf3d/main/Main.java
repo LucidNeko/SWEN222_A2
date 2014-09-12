49a80d09c4802d537e48384e2cc5cb1035386932
squd123 <prrism@192.168.0.10>
2014-09-12 18:31:20 +1200

    testing git

--- Wolf3D/src/wolf3d/main/Main.java
+++ Wolf3D/src/wolf3d/main/Main.java
@@ -5,7 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hellow world");
-		System.out.println("Goodbye world");
 	}
 
 }
