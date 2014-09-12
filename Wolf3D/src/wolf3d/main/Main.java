04ddc84b7fe46e7f9306ae226ea4aaaff471bc66
squd123 <prrism@192.168.0.10>
2014-09-12 18:27:33 +1200

    add printstatement

--- Wolf3D/src/wolf3d/main/Main.java
+++ Wolf3D/src/wolf3d/main/Main.java
@@ -1,11 +1,11 @@
 package wolf3d.main;
 
-public class main {
+public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hellow world");
-
+		System.out.println("Goodbye world");
 	}
 
 }
