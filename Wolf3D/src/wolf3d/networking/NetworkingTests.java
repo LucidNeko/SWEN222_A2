392fbe71a841abc54774d698630aad85b566ec94
JonesyMJ <mjnelson@gmx.com>
2014-09-12 18:53:43 +1200

    Some networking bones. It's not working properly yet, but it does listen correctly on server side, and a connection is made.

---
+++ Wolf3D/src/wolf3d/networking/NetworkingTests.java
@@ -0,0 +1,14 @@
+package wolf3d.networking;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NetworkingTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
