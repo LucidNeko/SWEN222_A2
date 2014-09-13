a6aeaf43675ee952993114405829c7b307104b4d
squd123 <prrism@192.168.0.10>
2014-09-13 13:30:24 +1200

    add few methods

--- Wolf3D/src/wolf3d/game/Player.java
+++ Wolf3D/src/wolf3d/game/Player.java
@@ -1,4 +1,8 @@
 package wolf3d.game;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * This class represents a player in the game world
  * @author Sameer Magan
@@ -6,6 +10,18 @@ package wolf3d.game;
  */
 public class Player {
 	private Vector pos;
+	private List<Item> items = new ArrayList<Item>();
 	
-
+	public Player(int x, int y, int z, float rotation){
+		pos = new Vector(x, y, z, rotation);
+	}
+	
+	/**
+	 * Adds an item to the items list
+	 * @param item to be added
+	 * @return true if successful false if not
+	 */
+	public boolean addItem(Item item){
+		return items.add(item);
+	}
 }
