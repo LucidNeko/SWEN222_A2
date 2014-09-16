b8dbfd2eb44d2a6eaff086bdebf3674054971579
squd123 <prrism@192.168.0.10>
2014-09-16 14:23:12 +1200

    Added createPlayer method to World class

--- Wolf3D/src/wolf3d/world/World.java
+++ Wolf3D/src/wolf3d/world/World.java
@@ -1,4 +1,12 @@
 package wolf3d.world;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import wolf3d.components.Transform;
+import wolf3d.core.Camera;
+import wolf3d.core.Entity;
+
 /**
  * This class is responsible for containing the state of the
  * world and the conditions and restrictions for the world
@@ -7,4 +15,14 @@ package wolf3d.world;
  */
 public class World {
 
+	private List<Entity> players = new ArrayList<Entity>();
+	
+	/**
+	 * Adds a new player to the game with given id
+	 */
+	public void createPlayer(int id){
+		Entity player = new Entity(id, Transform.class);
+		player.attachComponent(Camera.class);
+		players.add(player);
+	}
 }
