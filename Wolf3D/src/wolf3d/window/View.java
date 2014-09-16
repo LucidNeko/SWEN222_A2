18de91ae716511d16039d48943319e91ad732abc
LucidNeko <LucidNeko@users.noreply.github.com>
2014-09-16 22:33:57 +1200

    Added setCamera to the View interface.

--- Wolf3D/src/wolf3d/window/View.java
+++ Wolf3D/src/wolf3d/window/View.java
@@ -1,5 +1,7 @@
 package wolf3d.window;
 
+import wolf3d.core.Camera;
+
 /**
  * The View is an interface for a View that is to be displayed at a regular interval from a gameloop.
  * @author Hamish Rae-Hodgson
@@ -8,5 +10,8 @@ public interface View {
 	
 	/** Displays this view. A call to render itself. */
 	public void display();
+	
+	/** Sets the camera this view should use to render the world */
+	public void setCamera(Camera camera);
 
 }
