d06c71be8280ef99bfb5c8ebd4f484f05d58beaa
squd123 <prrism@192.168.0.10>
2014-09-16 14:09:18 +1200

    Created world package and class

---
+++ Wolf3D/src/wolf3d/world/World.java
@@ -0,0 +1,10 @@
+package wolf3d.world;
+/**
+ * This class is responsible for containing the state of the
+ * world and the conditions and restrictions for the world
+ * @author Sameer Magan
+ *
+ */
+public class World {
+
+}
