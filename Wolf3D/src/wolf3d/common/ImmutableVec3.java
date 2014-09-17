990a2cb2ad4e7d1fd6259c743663e27e6dae85b8
LucidNeko <LucidNeko@users.noreply.github.com>
2014-09-17 23:57:10 +1200

    Added javadoc to ImmutableVec3.

--- Wolf3D/src/wolf3d/common/ImmutableVec3.java
+++ Wolf3D/src/wolf3d/common/ImmutableVec3.java
@@ -11,14 +11,27 @@ public class ImmutableVec3 extends Vec3 {
 	// CONSTRUCTORS
 	//
 	
+	/**
+	 * Construct a new vector with (x, y, z) = (0, 0, 0).
+	 */
 	public ImmutableVec3() {
 		super();
 	}
 	
+	/**
+	 * Construct a new vector at (x, y, z).
+	 * @param x The x component of the vector.
+	 * @param y The y component of the vector.
+	 * @param z The z component of the vector.
+	 */
 	public ImmutableVec3(float x, float y, float z) {
 		super(x, y, z);
 	}
 	
+	/**
+	 * Construct a new vector such that (x, y, z) = (source.x, source.y, source.z).
+	 * @param source The vector to copy (x, y, z) from.
+	 */
 	public ImmutableVec3(Vec3 source) {
 		super(source);
 	}
@@ -27,16 +40,19 @@ public class ImmutableVec3 extends Vec3 {
 	// SET
 	//
 
+	/** Throws unsupported operation exception. */
 	@Override
 	public void set(Vec3 source) {
 		throw new UnsupportedOperationException("Cannot modify an immutable Vec3");
 	}
 	
+	/** Throws unsupported operation exception. */
 	@Override
 	public void set(float x, float y, float z) {
 		throw new UnsupportedOperationException("Cannot modify an immutable Vec3");
 	}
 	
+	/** Throws unsupported operation exception. */
 	@Override
 	public void setZero() {
 		throw new UnsupportedOperationException("Cannot modify an immutable Vec3");
@@ -46,11 +62,13 @@ public class ImmutableVec3 extends Vec3 {
 	// ADDITION
 	//
 	
+	/** Throws unsupported operation exception. */
 	@Override
 	public Vec3 addLocal(Vec3 other) {
 		throw new UnsupportedOperationException("Cannot modify an immutable Vec3");
 	}
 	
+	/** Throws unsupported operation exception. */
 	@Override
 	public Vec3 addLocal(float x, float y, float z) {
 		throw new UnsupportedOperationException("Cannot modify an immutable Vec3");
@@ -60,11 +78,13 @@ public class ImmutableVec3 extends Vec3 {
 	// SUBTRACTION
 	//
 	
+	/** Throws unsupported operation exception. */
 	@Override
 	public Vec3 subLocal(Vec3 other) {
 		throw new UnsupportedOperationException("Cannot modify an immutable Vec3");
 	}
 	
+	/** Throws unsupported operation exception. */
 	@Override
 	public Vec3 subLocal(float x, float y, float z) {
 		throw new UnsupportedOperationException("Cannot modify an immutable Vec3");
@@ -74,6 +94,7 @@ public class ImmutableVec3 extends Vec3 {
 	// MULTIPLICATION
 	//
 	
+	/** Throws unsupported operation exception. */
 	@Override
 	public Vec3 mulLocal(float factor) {
 		throw new UnsupportedOperationException("Cannot modify an immutable Vec3");
@@ -83,6 +104,7 @@ public class ImmutableVec3 extends Vec3 {
 	// DIVISION
 	//
 	
+	/** Throws unsupported operation exception. */
 	@Override
 	public Vec3 divLocal(float factor) {
 		throw new UnsupportedOperationException("Cannot modify an immutable Vec3");
@@ -92,16 +114,19 @@ public class ImmutableVec3 extends Vec3 {
 	// UTIL
 	//
 	
+	/** Throws unsupported operation exception. */
 	@Override
 	public Vec3 negateLocal() {
 		throw new UnsupportedOperationException("Cannot modify an immutable Vec3");
 	}
 	
+	/** Throws unsupported operation exception. */
 	@Override
 	public Vec3 absLocal() {
 		throw new UnsupportedOperationException("Cannot modify an immutable Vec3");
 	}	
 	
+	/** Throws unsupported operation exception. */
 	@Override
 	public float normalize() {
 		throw new UnsupportedOperationException("Cannot modify an immutable Vec3");
