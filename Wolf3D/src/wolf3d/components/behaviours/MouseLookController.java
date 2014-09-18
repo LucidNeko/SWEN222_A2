fffe0a0d7cf7702cdb2d3456ea5eb6a20f10234f
LucidNeko <LucidNeko@users.noreply.github.com>
2014-09-18 17:16:41 +1200

    Added in Behaviour components. Components that extend Behaviour should be updated each frame. Allows one to abstract away behaviour into components.

---
+++ Wolf3D/src/wolf3d/components/behaviours/MouseLookController.java
@@ -0,0 +1,37 @@
+package wolf3d.components.behaviours;
+
+import wolf3d.common.Mathf;
+import wolf3d.components.Transform;
+import wolf3d.core.Mouse;
+
+/**
+ * Adds Mouse look functionality to the entity.
+ * @author Hamish Rae-Hodgson
+ *
+ */
+public class MouseLookController extends Behaviour {
+
+	/** speed of looking around. Units per second. Low number for low dpi. */
+	private float speed = 40;
+	
+	/**
+	 * Set the speed that you look around at.
+	 * @param speed The speed.
+	 */
+	public void setSpeed(float speed) {
+		this.speed = speed;
+	}
+	
+	@Override
+	public void update(float delta) {
+		requires(Transform.class);
+		
+		Transform t = getOwner().getTransform();
+		
+		float dx = Mouse.getDX(); //calls to getDX/getDY wipe out the value.. TODO logical fix?
+		float dy = Mouse.getDY();
+		t.pitch(Mathf.degToRad(dy*speed*delta));
+		t.rotateY(Mathf.degToRad(dx*speed*delta)); 
+	}
+
+}
