caa919c48c1c94a288df1480879d4eadb17ff388
LucidNeko <LucidNeko@users.noreply.github.com>
2014-09-19 18:33:35 +1200

    Added javadoc to collision classes.

--- Wolf3D/src/wolf3d/components/AABB.java
+++ Wolf3D/src/wolf3d/components/AABB.java
@@ -1,5 +1,10 @@
 package wolf3d.components;
 
+/**
+ * The AABB class represents an Axis-Aligned Bounding Box.
+ * @author Hamish Rae-Hodgson
+ *
+ */
 public class AABB {
 
 }
--- Wolf3D/src/wolf3d/components/collision/Collider.java
+++ Wolf3D/src/wolf3d/components/collision/Collider.java
@@ -3,8 +3,17 @@ package wolf3d.components.collision;
 import wolf3d.components.AABB;
 import wolf3d.components.Component;
 
+/** 
+ * Defines a 3D collision shape.
+ * @author Hamish Rae-Hodgson
+ *
+ */
 public abstract class Collider extends Component {
 
+	/**
+	 * Gets the smallest AABB that fully encloses this Collider
+	 * @return The AABB.
+	 */
 	public abstract AABB getAABB();
 	
 }
