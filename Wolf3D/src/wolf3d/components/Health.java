6ecdaeebaa8ea02da6eb61bd814ca2a5fec903e7
magansame <magansame@city-limits.ecs.vuw.ac.nz>
2014-09-29 18:02:26 +1300

    Moved Heath to components

--- Wolf3D/src/wolf3d/components/Health.java
+++ Wolf3D/src/wolf3d/components/Health.java
@@ -1,30 +1,32 @@
-package wolf3d.components.behaviours;
+package wolf3d.components;
 
 import java.awt.event.KeyEvent;
 
 import engine.components.Behaviour;
+import engine.components.Sensor;
 import engine.input.Keyboard;
 
 /**
  * This class is responsible for maintaining the heath of the player
- * 
+ *
  * @author sameermagan
  *
  */
-public class Health extends Behaviour {
+public class Health extends Sensor{
 
 	private int health = 100;
 
 	private int damageAmt = 10;
 
 	@Override
-	public void update(float delta) {
+	public boolean isTriggered() {
 		if (Keyboard.isKeyDown(KeyEvent.VK_X)) {
 			if (health >= 0) {
 				health -= damageAmt;
 				System.out.println(health);
 			}
 		}
+		return false;
 	}
 
 }
