0d54ad229145fef475372b1a90a51da25e37acb5
squd123 <prrism@192.168.0.10>
2014-10-01 23:47:20 +1300

    Created TempEntityDef to make it easier for people like me who struggles working with generics

---
+++ Wolf3D/src/engine/core/TempEntityDef.java
@@ -0,0 +1,35 @@
+package engine.core;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import engine.components.Component;
+
+public class TempEntityDef {
+	private String name = "unnamed";
+	private List<Component> components = new ArrayList<Component>();
+
+	public TempEntityDef() {
+
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return the components
+	 */
+	public List<Component> getComponents() {
+		return components;
+	}
+	
+	public void addComponents(Collection<Component> components){
+		this.components.addAll(components);
+	}
+}
