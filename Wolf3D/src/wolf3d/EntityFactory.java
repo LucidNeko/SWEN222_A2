9e2b05e96ca42b9d71e7e93d17b528bfe01da674
Hamish <LucidNeko@users.noreply.github.com>
2014-10-02 09:02:30 +1300

    Added EntityFactory.

---
+++ Wolf3D/src/wolf3d/EntityFactory.java
@@ -0,0 +1,70 @@
+package wolf3d;
+
+import static javax.media.opengl.fixedfunc.GLLightingFunc.GL_LIGHT0;
+import static javax.media.opengl.fixedfunc.GLLightingFunc.GL_POSITION;
+
+import javax.media.opengl.GL2;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import wolf3d.components.Health;
+import wolf3d.components.Inventory;
+import wolf3d.components.behaviours.CameraScrollBackController;
+import wolf3d.components.behaviours.MouseLookController;
+import engine.components.Camera;
+import engine.components.MeshFilter;
+import engine.components.MeshRenderer;
+import engine.components.Renderer;
+import engine.core.Entity;
+import engine.core.World;
+import engine.texturing.Material;
+import engine.texturing.Mesh;
+import engine.texturing.Texture;
+import engine.util.Resources;
+
+public class EntityFactory {
+	private static final Logger log = LogManager.getLogger();
+	
+	public static final int PLAYER = 1;
+	
+	public static Entity create(int type, World world, String name) {
+		if(type == PLAYER) {
+			return createPlayer(world, name);
+		} else {
+			log.error("UnKnowen entity type {}", type);
+			log.error("Returning blank Entity");
+			return world.createEntity(name);
+		}
+	}
+	
+	public static Entity createPlayer(World world, String name) {
+		Mesh linkMesh = Resources.getMesh("link/young_link_s.obj");
+		Texture linkTex = Resources.getTexture("link/young_link.png", true);
+
+		//Create player
+		Entity player = world.createEntity("Player");
+		player.attachComponent(Camera.class);
+//		player.attachComponent(PyramidRenderer.class);
+		player.attachComponent(MeshFilter.class).setMesh(linkMesh);
+		player.attachComponent(MeshRenderer.class).setMaterial(new Material(linkTex));
+//		player.attachComponent(parser.getWallCollisionComponent());
+//		player.attachComponent(WASDWalking.class);
+		player.attachComponent(MouseLookController.class);
+		player.attachComponent(CameraScrollBackController.class);
+		player.attachComponent(Health.class);
+		//Testing pickup behavior
+		player.attachComponent(Inventory.class);
+		player.attachComponent(new Renderer() {
+
+			@Override
+			public void render(GL2 gl) {
+				//because renderering like a scenegraph (0,0,0) is transformed to the entities position.
+				gl.glLightfv(GL_LIGHT0, GL_POSITION, new float[] {0, 0, 0, 1}, 0); //1 signifies positional light
+			}
+
+		});
+		return player;
+	}
+
+}
