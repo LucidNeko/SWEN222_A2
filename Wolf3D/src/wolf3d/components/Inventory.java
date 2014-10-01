ec5edd9306e14fe93a75dacee51d0d72cc867eec
squd123 <prrism@192.168.0.10>
2014-10-02 01:56:45 +1300

    Added Some javadoc to Inventory.class

--- Wolf3D/src/wolf3d/components/Inventory.java
+++ Wolf3D/src/wolf3d/components/Inventory.java
@@ -5,7 +5,12 @@ import java.util.List;
 
 import engine.components.Component;
 import engine.core.Entity;
-
+/**
+ * This class is responsible for holding all of the items that a player
+ * has picked up
+ * @author Sameer Magan
+ *
+ */
 public class Inventory extends Component {
 	List<Entity> items = new ArrayList<Entity>();
 
