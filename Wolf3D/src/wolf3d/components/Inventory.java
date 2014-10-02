2e65380a4aed71b9e50bc7c436f0de19c7333dab
squd123 <prrism@192.168.0.10>
2014-10-02 20:46:12 +1300

    Added a get method to return a specific element in the items array

--- Wolf3D/src/wolf3d/components/Inventory.java
+++ Wolf3D/src/wolf3d/components/Inventory.java
@@ -5,30 +5,39 @@ import java.util.List;
 
 import engine.components.Component;
 import engine.core.Entity;
+
 /**
- * This class is responsible for holding all of the items that a player
- * has picked up
+ * This class is responsible for holding all of the items that a player has
+ * picked up
+ * 
  * @author Sameer Magan
  *
  */
 public class Inventory extends Component {
 	private List<Entity> items = new ArrayList<Entity>();
-	
+
 	private int carryWeight = 100;
 
 	public List<Entity> getItems() {
 		return items;
 	}
 
-	public boolean addItem(Entity item){
+	public boolean addItem(Entity item) {
 		return items.add(item);
 	}
 
-	public boolean removeEntity(Entity item){
+	public Entity get(int index) {
+		if (index >= items.size()) {
+			return null;
+		}
+		return items.get(index);
+	}
+
+	public boolean removeEntity(Entity item) {
 		return items.remove(item);
 	}
-	
-	public boolean contains(Entity item){
+
+	public boolean contains(Entity item) {
 		return items.contains(item);
 	}
 
@@ -36,18 +45,16 @@ public class Inventory extends Component {
 		return carryWeight;
 	}
 
-	public boolean reduceCarryWeight(int weight){
-		if((carryWeight - weight) >= 0){
+	public boolean reduceCarryWeight(int weight) {
+		if ((carryWeight - weight) >= 0) {
 			carryWeight -= weight;
 			return true;
 		}
 		return false;
 	}
-	
-	public void releaseWeight(int weight){
+
+	public void releaseWeight(int weight) {
 		carryWeight += weight;
 	}
-	
-
 
 }
