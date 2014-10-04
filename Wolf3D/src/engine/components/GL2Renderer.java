f3801f7406f4952bc7ef763782ea5e21ff1b86dc
Hamish <LucidNeko@users.noreply.github.com>
2014-10-04 21:33:54 +1300

    Fixed Javadoc in GL2Renderer.

--- Wolf3D/src/engine/components/GL2Renderer.java
+++ Wolf3D/src/engine/components/GL2Renderer.java
@@ -3,7 +3,7 @@ package engine.components;
 import javax.media.opengl.GL2;
 
 /**
- * An interface that all renderable components must implement
+ * An interface that all OpenGL renderable components must implement
  * @author Hamish
  *
  */
