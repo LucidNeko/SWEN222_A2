94f8d736d54b5f0b291f77f108d6acec27bee8a4
squd123 <prrism@192.168.0.10>
2014-10-05 17:23:17 +1300

    Added Attackable Component so that a player can now attack an enemy by pressing X

---
+++ Wolf3D/src/wolf3d/components/behaviours/Attackable.java
@@ -0,0 +1,43 @@
+package wolf3d.components.behaviours;
+
+import wolf3d.components.Health;
+import wolf3d.components.sensors.ProximitySensor;
+
+import com.jogamp.newt.event.KeyEvent;
+
+import engine.components.Behaviour;
+import engine.core.Entity;
+import engine.core.World;
+import engine.input.Keyboard;
+
+public class Attackable extends Behaviour{
+	private World world;
+	
+	public Attackable(World world){
+		this.world = world;
+	}
+	
+	private void attack(){
+		Entity player = getOwner().getComponent(ProximitySensor.class).getTarget();
+		Health playerHealth = player.getComponent(Health.class);
+		Health enemyHealth = getOwner().getComponent(Health.class);
+		
+		if(!enemyHealth.decreaseHealth(playerHealth.getDamageAmt())){
+			world.destroyEntity(getOwner().getID());
+		}
+	}
+
+	@Override
+	public void update(float delta) {
+		requires(Health.class);
+		requires(ProximitySensor.class);
+		if(getOwner().getComponent(ProximitySensor.class).isTriggered()){
+			if(Keyboard.isKeyDownOnce(KeyEvent.VK_C)){
+				attack();
+				System.out.println("C has been pressed!");
+			}
+		}
+		
+	}
+
+}
