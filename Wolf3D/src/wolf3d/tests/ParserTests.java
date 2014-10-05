2376ec4755070f1e24ac68aca7cfe4635af813ff
squd123 <prrism@192.168.0.10>
2014-10-05 19:45:57 +1300

    Added JUnit tests for Parser

---
+++ Wolf3D/src/wolf3d/tests/ParserTests.java
@@ -0,0 +1,24 @@
+package wolf3d.tests;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import wolf3d.world.Parser;
+import engine.core.World;
+
+public class ParserTests {
+	private Parser parser;
+	private World world;
+	
+	@Before
+	public void init(){
+		parser = new Parser("Map.txt", "Doors.txt");
+		world = new World();
+	}
+	
+	@Test
+	public void test_passWallFileToArray(){
+		parser.passWallFileToArray();
+		parser.createWalls(world);
+	}
+}
