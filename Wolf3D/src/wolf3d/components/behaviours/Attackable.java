9532a4d4d50dd7809f664e8bbbf0bdcd7f1d6c3c
squd123 <prrism@192.168.0.10>
2014-10-05 17:47:52 +1300

    Added som javadoc to Attackable.class

--- Wolf3D/src/wolf3d/components/behaviours/Attackable.java
+++ Wolf3D/src/wolf3d/components/behaviours/Attackable.java
@@ -10,6 +10,12 @@ import engine.core.Entity;
 import engine.core.World;
 import engine.input.Keyboard;
 
+/**
+ * This class is responsible for allowing the owner of this Component to be 
+ * attacked when they are in a certain proximity of a target
+ * @author Sameer Magan
+ *
+ */
 public class Attackable extends Behaviour{
 	private World world;
 	
@@ -17,11 +23,16 @@ public class Attackable extends Behaviour{
 		this.world = world;
 	}
 	
+	/**
+	 * Attacks the Owner of this component by the damage amount of the player
+	 * attacking
+	 */
 	private void attack(){
 		Entity player = getOwner().getComponent(ProximitySensor.class).getTarget();
 		Health playerHealth = player.getComponent(Health.class);
 		Health enemyHealth = getOwner().getComponent(Health.class);
 		
+		//checks if player is dead or alive
 		if(!enemyHealth.decreaseHealth(playerHealth.getDamageAmt())){
 			world.destroyEntity(getOwner().getID());
 		}
@@ -32,7 +43,7 @@ public class Attackable extends Behaviour{
 		requires(Health.class);
 		requires(ProximitySensor.class);
 		if(getOwner().getComponent(ProximitySensor.class).isTriggered()){
-			if(Keyboard.isKeyDownOnce(KeyEvent.VK_X)){
+			if(Keyboard.isKeyDownOnce(KeyEvent.VK_C)){
 				attack();
 				System.out.println("C has been pressed!");
 			}
