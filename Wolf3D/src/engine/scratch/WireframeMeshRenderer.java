3b90be96fd85889c3ddb4a459c20329c1e1d4215
Hamish <LucidNeko@users.noreply.github.com>
2014-10-06 14:56:05 +1300

    Added WireframeMeshRenderer.

---
+++ Wolf3D/src/engine/scratch/WireframeMeshRenderer.java
@@ -0,0 +1,40 @@
+package engine.scratch;
+
+import java.util.Iterator;
+
+import javax.media.opengl.GL2;
+
+import engine.common.Vec3;
+import engine.components.GL2Renderer;
+import engine.components.MeshFilter;
+import engine.texturing.Mesh;
+
+public class WireframeMeshRenderer extends GL2Renderer {
+
+	@Override
+	public void render(GL2 gl) {
+		if(!requires(MeshFilter.class)) return;
+
+		Mesh mesh = getOwner().getComponent(MeshFilter.class).getMesh();
+
+		gl.glColor3f(0, 0, 0);
+		Iterator<Vec3> iter = mesh.iterator();
+		while(iter.hasNext()) {
+			Vec3 a = iter.next();
+			Vec3 b = iter.next();
+			Vec3 c = iter.next();
+
+			gl.glBegin(GL2.GL_LINES);
+				gl.glVertex3f(a.x(), a.y(), a.z());
+				gl.glVertex3f(b.x(), b.y(), b.z());
+
+				gl.glVertex3f(b.x(), b.y(), b.z());
+				gl.glVertex3f(c.x(), c.y(), c.z());
+
+				gl.glVertex3f(c.x(), c.y(), c.z());
+				gl.glVertex3f(a.x(), a.y(), a.z());
+			gl.glEnd();
+		}
+	}
+
+}
