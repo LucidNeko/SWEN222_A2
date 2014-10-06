bbec1bfb12113fe0fb73c232676d5a200ee7dd90
Hamish <LucidNeko@users.noreply.github.com>
2014-10-06 19:27:56 +1300

    Removed 48mb skybox..

--- Wolf3D/src/wolf3d/GameDemo.java
+++ Wolf3D/src/wolf3d/GameDemo.java
@@ -126,7 +126,7 @@ public class GameDemo extends GameLoop {
 		
 		Entity skybox = world.createEntity("skybox");
 		skybox.attachComponent(MeshFilter.class).setMesh(Resources.getMesh("skybox.obj"));
-		skybox.attachComponent(LightlessMeshRenderer.class).setMaterial(new Material(Resources.getTexture("skybox3.png", true)));
+		skybox.attachComponent(LightlessMeshRenderer.class).setMaterial(new Material(Resources.getTexture("skybox2.jpg", true)));
 		skybox.attachComponent(new Behaviour() {
 			//moves the box around with player so they can't come close to the edges.
 			@Override
