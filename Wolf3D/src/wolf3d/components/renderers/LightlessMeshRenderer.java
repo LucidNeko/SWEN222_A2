6f0476ec39eb11b37683b42c23fb97112d328327
Hamish <LucidNeko@users.noreply.github.com>
2014-10-06 18:53:52 +1300

    Added a MeshRenderer that disables then enables light when it renders. useful for lightbox.

---
+++ Wolf3D/src/wolf3d/components/renderers/LightlessMeshRenderer.java
@@ -0,0 +1,48 @@
+package wolf3d.components.renderers;
+
+import javax.media.opengl.GL2;
+
+import engine.components.GL2Renderer;
+import engine.components.MeshFilter;
+import engine.texturing.Material;
+import engine.texturing.Mesh;
+
+/**
+ * 
+ * @author Hamish
+ *
+ */
+public class LightlessMeshRenderer extends GL2Renderer {
+	
+	private Material material = new Material();
+	
+	/**
+	 * Sets the Material that this MeshRenderer uses.
+	 * @param material The Material. Cannot be null.
+	 */
+	public void setMaterial(Material material) {
+		if(material == null) throw new IllegalArgumentException("Cannot set material to null.");
+		this.material = material;
+	}
+
+	@Override
+	public void render(GL2 gl) {
+		requires(MeshFilter.class);
+		
+		Mesh mesh = getOwner().getComponent(MeshFilter.class).getMesh();
+		if(mesh == null) return;
+
+		gl.glDisable(GL2.GL_LIGHTING);
+		
+		material.bind(gl);
+		mesh.bind(gl);
+		mesh.draw(gl);
+		mesh.unbind(gl);
+		material.unbind(gl);
+
+		//FIXME: WARNING WILL ENABLE LIGHTING IF NOT ENABLED.
+		gl.glEnable(GL2.GL_LIGHTING);
+		
+	}
+
+}
