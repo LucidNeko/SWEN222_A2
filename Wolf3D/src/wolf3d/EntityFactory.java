fac0f20845ee13af26089e28cbf62442d7d3c382
Hamish <LucidNeko@users.noreply.github.com>
2014-10-07 19:58:06 +1300

    Reoved why look in 3rd person

--- Wolf3D/src/wolf3d/EntityFactory.java
+++ Wolf3D/src/wolf3d/EntityFactory.java
@@ -33,15 +33,15 @@ import engine.texturing.Texture;
 import engine.util.Resources;
 
 /**
- * 
+ *
  * @author Hamish
  *
  */
 public class EntityFactory {
 	private static final Logger log = LogManager.getLogger();
-	
+
 	public static final int PLAYER = 1;
-	
+
 	public static Entity create(int type, World world, String name) {
 		if(type == PLAYER) {
 			return createPlayer(world, name);
@@ -51,7 +51,7 @@ public class EntityFactory {
 			return world.createEntity(name);
 		}
 	}
-	
+
 	public static Entity createPlayer(World world, String name) {
 		Mesh linkMesh = Resources.getMesh("link/young_link_s.obj");
 		Texture linkTex = Resources.getTexture("link/young_link.png", true);
@@ -70,7 +70,7 @@ public class EntityFactory {
 		player.attachComponent(new DropItem(world));
 		return player;
 	}
-	
+
 	public static Entity createFirstPersonCamera(World world, final Entity target) {
 		Entity camera = world.createEntity("Camera");
 		camera.attachComponent(new Behaviour() {
@@ -79,16 +79,16 @@ public class EntityFactory {
 			public void update(float delta) {
 				Transform at = target.getTransform();
 				Transform cam = getOwner().getTransform();
-				
+
 				cam.setPosition(at.getPosition());
 //				cam.lookInDirection(at.getLook());
 				cam.walk(0.25f);
-				
+
 				float dx = Mouse.getDX(); //calls to getDX/getDY wipe out the value.. TODO logical fix?
 				float dy = Mouse.getDY();
 				cam.pitch(Mathf.degToRad(dy*10*delta));
-				cam.rotateY(Mathf.degToRad(dx*10*delta)); 
-				
+				cam.rotateY(Mathf.degToRad(dx*10*delta));
+
 				at.lookInDirection(cam.getLook());
 
 //				cam.lookInDirection(at.getPosition().sub(cam.getPosition()));
@@ -101,12 +101,12 @@ public class EntityFactory {
 //				camLook.setY(0);
 //				camLook.normalize();
 //				float dot = atLook.x()*(-camLook.z()) + atLook.z()*camLook.x();
-//				
+//
 //				float sign = dot < 0 ? -1: 1;
-//				
-//				if(Vec3.dot(camLook, atLook) < 0.99f) cam.strafe(sign*10*Mathf.abs(dot)*delta); 
+//
+//				if(Vec3.dot(camLook, atLook) < 0.99f) cam.strafe(sign*10*Mathf.abs(dot)*delta);
 			}
-			
+
 		});
 		camera.attachComponent(Camera.class);
 		camera.attachComponent(PyramidRenderer.class);
@@ -119,12 +119,12 @@ public class EntityFactory {
 				Vec3 look = t.getLook();
 				gl.glLightfv(GL_LIGHT1, GL_POSITION, new float[] {0, 0, 0, 1}, 0); //1 signifies positional light
 				gl.glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, new float[]{ 0,0,1}, 0); //light direction is forwards
-			} 
+			}
 
 		});
 		return camera;
 	}
-	
+
 	public static Entity createThirdPersonTrackingCamera(World world, final Entity target) {
 		final Entity camera = world.createEntity("Camera");
 		final float near = 0.8f; //0.8f;
@@ -135,13 +135,13 @@ public class EntityFactory {
 			public void update(float delta) {
 				Transform at = target.getTransform();
 				Transform cam = getOwner().getTransform();
-				
-				float dy = Mouse.getDY();
-				cam.fly(dy*0.5f*delta);
-				cam.lookAt(at);
-				
-//				cam.lookInDirection(at.getPosition().sub(cam.getPosition()));
-				
+
+//				float dy = Mouse.getDY();
+//				cam.fly(dy*0.5f*delta);
+//				cam.lookAt(at);
+
+				cam.lookInDirection(at.getPosition().sub(cam.getPosition()));
+
 				float length = cam.getPosition().sub(at.getPosition()).length();
 				if(length > far) cam.walk(7*(1-(1/length))*delta);
 				else if(length < near) cam.walk(-2*delta);
@@ -152,17 +152,17 @@ public class EntityFactory {
 				camLook.setY(0);
 				camLook.normalize();
 				float dot = atLook.x()*(-camLook.z()) + atLook.z()*camLook.x();
-				
+
 				float sign = dot < 0 ? -1: 1;
-				
+
 				float theta = (float) Math.acos(Vec3.dot(atLook, camLook));
-				
-				//if(Vec3.dot(camLook, atLook) < 0.995f) cam.strafe(sign*10*Mathf.abs(dot)*delta); 
+
+				//if(Vec3.dot(camLook, atLook) < 0.995f) cam.strafe(sign*10*Mathf.abs(dot)*delta);
 				if(Mathf.radToDeg(theta) > 1) {
 					cam.strafe(10*sign*theta*delta);
 				}
 			}
-			
+
 		});
 		camera.attachComponent(Camera.class);
 		camera.attachComponent(PyramidRenderer.class);
@@ -175,11 +175,11 @@ public class EntityFactory {
 				Vec3 look = t.getLook();
 				gl.glLightfv(GL_LIGHT1, GL_POSITION, new float[] {0, 0, 0, 1}, 0); //1 signifies positional light
 				gl.glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, new float[]{ 0,0,1}, 0); //light direction is forwards
-			} 
+			}
 
 		});
 		camera.getTransform().setPosition(target.getTransform().getPosition().add(target.getTransform().getLook().mul(25))); //start far away so it zooms in
-		
+
 		target.attachComponent(new Behaviour() {
 
 			@Override
@@ -187,14 +187,14 @@ public class EntityFactory {
 				float dx = Mouse.getDX(); //calls to getDX/getDY wipe out the value.. TODO logical fix?
 //				float dy = Mouse.getDY();
 //				target.getTransform().pitch(Mathf.degToRad(dy*10*delta));
-				target.getTransform().rotateY(Mathf.degToRad(dx*10*delta)); 
+				target.getTransform().rotateY(Mathf.degToRad(dx*10*delta));
 			}
-			
+
 		});
-		
+
 		return camera;
 	}
-	
+
 	public static Entity createSun(World world) {
 		final Vec3 target = new Vec3(10, 0, 10);
 		Entity sun = world.createEntity("Sun");
