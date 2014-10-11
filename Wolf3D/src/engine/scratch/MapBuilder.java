e5d8a4db8270b16a2a8a3729073567c4d35c9971
Hamish <LucidNeko@users.noreply.github.com>
2014-10-11 15:29:34 +1300

    Changed MapBUilder to place walls as double sided. Now no need to add both sides manually.

--- Wolf3D/src/engine/scratch/MapBuilder.java
+++ Wolf3D/src/engine/scratch/MapBuilder.java
@@ -7,10 +7,15 @@ import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.util.Scanner;
 import java.util.concurrent.locks.LockSupport;
 
 import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
@@ -26,6 +31,7 @@ import engine.input.Keyboard;
 public class MapBuilder extends JFrame {
 	private static final long serialVersionUID = -9166187539200705700L;
 	
+	private static final int NONE  = 0x0;
 	private static final int WEST  = 0x1;
 	private static final int SOUTH = 0x2;
 	private static final int EAST  = 0x4;
@@ -88,9 +94,38 @@ public class MapBuilder extends JFrame {
 		loadButton.setFocusable(false);
 		loadButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				JFileChooser jfc = null;
+				try {
+					jfc = new JFileChooser(new File(MapBuilder.class.getResource("/wolf3d/assets/").toURI()));
+				} catch (URISyntaxException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				jfc.showDialog(MapBuilder.this, "Choose");
+				File f = jfc.getSelectedFile();
+				if(f != null) {
+					try {
+						Scanner scan = new Scanner(f);
+						int cols = Integer.parseInt(scan.nextLine());
+						int rows = Integer.parseInt(scan.nextLine());
+						cells = new int[rows][cols];
+						for(int row = 0; row < rows; row++) {
+							String[] line = scan.nextLine().split(""); //jdk7 vs 8
+							for(int col = 0; col < cols; col++) {
+								cells[row][col] = Integer.decode("0x"+line[col]);
+							}
+						}
+						MapBuilder.this.repaint();
+					} catch (FileNotFoundException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
 				
 			}
 		});
+		buttonPanel.add(loadButton);
+		
 		this.getContentPane().add(buttonPanel, BorderLayout.NORTH);
 		
 		this.getContentPane().add(new JComponent() {
@@ -145,27 +180,48 @@ public class MapBuilder extends JFrame {
 						selectionY = (int) Mathf.clamp(selectionY+dy, 0, MAP_HEIGHT-1);
 					}
 					
-					int cell = cells[selectionY][selectionX];
-					if(Keyboard.isKeyDownOnce(KeyEvent.VK_W)) {
-						if((cell & NORTH) != 0) 
-							cells[selectionY][selectionX] &= ~NORTH; //remove
-						else cells[selectionY][selectionX] |= NORTH; //add
+					int walls = 0;
+					if(Keyboard.isKeyDownOnce(KeyEvent.VK_W)) walls |= NORTH;
+					if(Keyboard.isKeyDownOnce(KeyEvent.VK_A)) walls |= WEST;
+					if(Keyboard.isKeyDownOnce(KeyEvent.VK_S)) walls |= SOUTH;
+					if(Keyboard.isKeyDownOnce(KeyEvent.VK_D)) walls |= EAST;
+					
+					//TODO:
+					if(walls != NONE) {
+						cells[selectionY][selectionX] ^= walls; //this
+						if(selectionX > 0)
+							cells[selectionY][selectionX-1] ^= opposite(WEST & walls); //left
+						if(selectionX < MAP_WIDTH-1)
+							cells[selectionY][selectionX+1] ^= opposite(EAST & walls); //right
+						if(selectionY > 0)
+							cells[selectionY-1][selectionX] ^= opposite(NORTH & walls); //up
+						if(selectionY < MAP_HEIGHT-1)
+							cells[selectionY+1][selectionX] ^= opposite(SOUTH & walls); //down
 					}
-					if(Keyboard.isKeyDownOnce(KeyEvent.VK_A)) {
-						if((cell & WEST) != 0) 
-							cells[selectionY][selectionX] &= ~WEST;
-						else cells[selectionY][selectionX] |= WEST;
-					}
-					if(Keyboard.isKeyDownOnce(KeyEvent.VK_S)) {
-						if((cell & SOUTH) != 0) 
-							cells[selectionY][selectionX] &= ~SOUTH;
-						else cells[selectionY][selectionX] |= SOUTH;
-					}
-					if(Keyboard.isKeyDownOnce(KeyEvent.VK_D)) {
-						if((cell & EAST) != 0) 
-							cells[selectionY][selectionX] &= ~EAST;
-						else cells[selectionY][selectionX] |= EAST;
-					}
+					
+					//this cell
+//					if()
+//					
+//					if(Keyboard.isKeyDownOnce(KeyEvent.VK_W)) {
+//						if((cell & NORTH) != 0) 
+//							cells[selectionY][selectionX] &= ~NORTH; //remove
+//						else cells[selectionY][selectionX] |= NORTH; //add
+//					}
+//					if(Keyboard.isKeyDownOnce(KeyEvent.VK_A)) {
+//						if((cell & WEST) != 0) 
+//							cells[selectionY][selectionX] &= ~WEST;
+//						else cells[selectionY][selectionX] |= WEST;
+//					}
+//					if(Keyboard.isKeyDownOnce(KeyEvent.VK_S)) {
+//						if((cell & SOUTH) != 0) 
+//							cells[selectionY][selectionX] &= ~SOUTH;
+//						else cells[selectionY][selectionX] |= SOUTH;
+//					}
+//					if(Keyboard.isKeyDownOnce(KeyEvent.VK_D)) {
+//						if((cell & EAST) != 0) 
+//							cells[selectionY][selectionX] &= ~EAST;
+//						else cells[selectionY][selectionX] |= EAST;
+//					}
 					
 					MapBuilder.this.repaint();
 					
@@ -174,6 +230,15 @@ public class MapBuilder extends JFrame {
 				}
 			}
 		}).start();
+	}
+	
+	private int opposite(int flag) {
+		if(flag == NONE) return NONE;
+		if(flag == NORTH) return SOUTH;
+		if(flag == SOUTH) return NORTH;
+		if(flag == EAST) return WEST;
+		if(flag == WEST) return EAST;
+		throw new IllegalArgumentException(Integer.toHexString(flag));
 	}
 	
 	public static void main(String[] args) {
