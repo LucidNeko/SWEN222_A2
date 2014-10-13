0410843af37db886dde61f301136d0201256340e
SimonB <simonbrannigan@gmail.com>
2014-10-13 17:33:11 +1300

    Start of the test class

---
+++ Wolf3D/src/engine/util/WolfTests.java
@@ -0,0 +1,14 @@
+package engine.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WolfTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
