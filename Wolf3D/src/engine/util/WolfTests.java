b1ca85d71d9b91f39200ec668dc6664a5b175a45
SimonB <simonbrannigan@gmail.com>
2014-10-13 19:10:18 +1300

    Comments in test classes

--- Wolf3D/src/engine/util/WolfTests.java
+++ Wolf3D/src/engine/util/WolfTests.java
@@ -1,3 +1,9 @@
+/**
+ * @author Simon Brannigan
+ *
+ *
+ */
+
 package engine.util;
 
 import static org.junit.Assert.*;
@@ -6,9 +12,176 @@ import org.junit.Test;
 
 public class WolfTests {
 
+	/*
+	 * Ensure that the package has all the items and textures that it needs
+	 * */
+
+	// .txt files present
 	@Test
-	public void test() {
+	public void packageTest1() {
+		//If no wall map, no textures and no doors. Fail.
 		fail("Not yet implemented");
 	}
 
+	// Model files present in the folder
+	@Test
+	public void packageTest2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void packageTest3() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void packageTest4() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void packageTest5() {
+		fail("Not yet implemented");
+	}
+
+	/*
+	 * Map creation tests
+	 * */
+
+	@Test
+	public void creationTest1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void creationTest2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void creationTest3() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void creationTest4() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void creationTest5() {
+		fail("Not yet implemented");
+	}
+
+	/*
+	 * Database Tests
+	 * */
+
+	@Test
+	public void databaseTest1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void databaseTest2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void databaseTest3() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void databaseTest4() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void databaseTest5() {
+		fail("Not yet implemented");
+	}
+
+	/*
+	 * Network Tests
+	 * */
+
+	@Test
+	public void networkTest1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void networkTest2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void networkTest3() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void networkTest4() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void networkTest5() {
+		fail("Not yet implemented");
+	}
+
+	/*
+	 * Game logic tests
+	 * */
+
+	@Test
+	public void logicTest1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void logicTest2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void logicTest3() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void logicTest4() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void logicTest5() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void logicTest6() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void logicTest7() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void logicTest8() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void logicTest9() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void logicTest10() {
+		fail("Not yet implemented");
+	}
 }
