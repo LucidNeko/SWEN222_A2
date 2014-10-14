0408dd38819485f6897f6e490982595f2638f64e
squd123 <prrism@192.168.0.10>
2014-10-14 13:12:31 +1300

    Added World to SpecialDoorBehaviour

--- Wolf3D/src/wolf3d/components/behaviours/SpecialDoorBehaviour.java
+++ Wolf3D/src/wolf3d/components/behaviours/SpecialDoorBehaviour.java
@@ -4,15 +4,17 @@ import java.awt.event.KeyEvent;
 
 import wolf3d.components.sensors.ProximitySensor;
 import engine.components.Behaviour;
+import engine.core.World;
 import engine.input.Keyboard;
 
 public class SpecialDoorBehaviour extends Behaviour {
+	private World world;
 
 	@Override
 	public void update(float delta) {
 		if (Keyboard.isKeyDown(KeyEvent.VK_SPACE)) {
 			if (getOwner().getComponent(ProximitySensor.class).isTriggered()) {
-
+				
 			}
 		}
 
