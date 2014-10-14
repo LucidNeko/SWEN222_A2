86725382cc8431fcf530a097ff82ca7e79116366
Hamish <LucidNeko@users.noreply.github.com>
2014-10-14 23:43:32 +1300

    Added author to Messenger.

--- Wolf3D/src/engine/util/Messenger.java
+++ Wolf3D/src/engine/util/Messenger.java
@@ -5,6 +5,11 @@ import javax.swing.JLabel;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ *
+ * @author Hamish
+ *
+ */
 public class Messenger implements Service {
 	private static final Logger log = LogManager.getLogger();
 
