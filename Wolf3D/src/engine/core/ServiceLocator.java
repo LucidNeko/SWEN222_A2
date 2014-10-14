e30858a66b91952b437b5e870bea5be341548f45
magansame <magansame@vurtigo.ecs.vuw.ac.nz>
2014-10-14 18:00:39 +1300

    Added JavaDoc to ServiceLocator

--- Wolf3D/src/engine/core/ServiceLocator.java
+++ Wolf3D/src/engine/core/ServiceLocator.java
@@ -3,6 +3,12 @@ package engine.core;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * This Locates and returns the service and instantiates and saves
+ * the service to the cache if there is no service in the cache
+ * @author Sameer Magan 300223776
+ *
+ */
 public class ServiceLocator {
 	private static final Logger log = LogManager.getLogger();
 
@@ -12,23 +18,13 @@ public class ServiceLocator {
 		cache = new Cache();
 	}
 
+	/**
+	 * This gets the instantiated service provided, if there is not already an
+	 * instantiated service it instantiates it and returns
+	 * @param service the service that you want to get
+	 * @return the instantiated service
+	 */
 	public static <E extends Service> E getService(Class<? extends E> service) {
-
 		return cache.getService(service);
-
 	}
-//
-//	public static <E extends Service> E getService(Class<? extends E> service) {
-//		try {
-//			return service.newInstance();
-//		} catch (InstantiationException | IllegalAccessException e) {
-//			log.error("Failed instantiating an instance of {}", service.getSimpleName());
-//			throw new Error(e);
-//		}
-//	}
-
-	public static void main(String[] args) {
-		World world = ServiceLocator.getService(World.class);
-	}
-
 }
