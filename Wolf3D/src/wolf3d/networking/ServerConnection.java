0535c7e2d66af74fe86145cf8398f3223ce0b01c
Michael Nelson <mjnelson@gmx.com>
2014-10-14 13:20:19 +1300

    Added JDocs and removed lots of old code

--- Wolf3D/src/wolf3d/networking/ServerConnection.java
+++ Wolf3D/src/wolf3d/networking/ServerConnection.java
@@ -5,16 +5,24 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
+/**
+ * @author Michael Nelson (300276118)
+ */
 public class ServerConnection extends Thread{
 	private Socket soc;
+
 	private DataInputStream in;
 	private DataOutputStream out;
 
-	private Server master;
 
-	public ServerConnection(Socket socket, Server master){
+	/**
+	 * Constructor for ServerConnection class.
+	 * Each ServerConnection has input and output streams for communicating with a client
+	 *
+	 * @param socket The socket on which this connection should listen.
+	 */
+	public ServerConnection(Socket socket){
 		soc=socket;
-		this.master = master;
 		try {
 			out = new DataOutputStream(soc.getOutputStream());
 			in = new DataInputStream(soc.getInputStream());
@@ -24,44 +32,18 @@ public class ServerConnection extends Thread{
 		}
 	}
 
-	
+	/**
+	 * This method returns true if the socket is still open and connected.
+	 * @return
+	 */
 	public boolean areWeAlive(){
 		return (!(soc.isClosed()) && soc.isConnected());
 	}
-	
+
 	public void run(){
 		try{
-			
-			//try{
-
-
 			while(true){
-				/*
-				if(in.available()>0){
-					System.out.println("HELLO");
-					String marker = in.readUTF();
-
-					if(marker.equals("transform")){
-						master.pushToAllClients("transform"); //marker
-						master.pushToAllClients(in.readInt()); //ID of entity transformed.
-						master.pushToAllClients(in.readUTF()); //json string of transform.
-					}
-					if(marker.equals("message")){
-						master.pushToAllClients("message");
-						master.pushToAllClients(in.readInt()); //ID of sender.
-						master.pushToAllClients(in.readUTF()); // message itself.
-					}
-				}
-				*/
-				
 			}
-
-			/*
-			}catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			*/
 		}
 		finally{
 			try {
@@ -73,58 +55,48 @@ public class ServerConnection extends Thread{
 		}
 	}
 
-	//
-	//TODO
-	//MARK FOR DELETION.
-	public void pushToClient(byte[] message){
-		try {
-			int length = message.length;
-			out.toString();
-			out.writeInt(length);
-			if (length > 0) {
-				out.write(message);
-			}} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	}
-
+	/**
+	 * Send a string to the client.
+	 * @param string UTF string.
+	 */
 	public void pushToClient(String string) {
-		// TODO Auto-generated method stub
 		try {
 			out.writeUTF(string);
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 	}
 
+	/**
+	 * Send an int to the client.
+	 * @param i
+	 */
 	public void pushToClient(int i) {
-		// TODO Auto-generated method stub
-
 		try {
 			out.writeInt(i);
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 	}
 
-	public DataInputStream getInputStream() {
-		// TODO Auto-generated method stub
-		return in;
-	}
-
-
+	/**
+	 * Send a float to the client.
+	 * @param f
+	 */
 	public void pushToClient(float f) {
-		// TODO Auto-generated method stub
 		try {
 			out.writeFloat(f);
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 	}
 
+	/**
+	 * Return the DataInputStream that this ServerConnection holds.
+	 * @return
+	 */
+	public DataInputStream getInputStream() {
+		return in;
+	}
 
 }
