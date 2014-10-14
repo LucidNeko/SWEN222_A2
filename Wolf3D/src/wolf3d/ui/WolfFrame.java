76946a1d7d5f58373990fdc0ecb352377927f900
SimonB <simonbrannigan@gmail.com>
2014-10-14 20:18:24 +1300

    Tidied up the hit boxes in wolfFrame so hit boxes no longer so arbitrary.

--- Wolf3D/src/wolf3d/ui/WolfFrame.java
+++ Wolf3D/src/wolf3d/ui/WolfFrame.java
@@ -28,7 +28,7 @@ public class WolfFrame extends JFrame implements MouseListener{
 		f.setSize(width, height);
 		f.setTitle("Wolf3D");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setResizable(true); // may replace this to be resizable but the pic may look weird
+		f.setResizable(false); // may replace this to be resizable but the pic may look weird
 
 		// or just have two sizes, full screen and 800 by 600
 		f.add(new WolfCanvas());
@@ -39,30 +39,32 @@ public class WolfFrame extends JFrame implements MouseListener{
 
 	/**
 	 * @author brannisimo
-	 * Takes the x and y and checks if they are near a selection.
+	 * @param x
+	 * @param y
+	 * @return Takes the x and y and checks if they are near a selection.
 	 */
 	private void hitBox(int x, int y){
 		// TODO
 		/* Make the numbers not hard coded and tweak the pic till I can
-		 * have a basic hit box algorithm*/
+		 * have a basic hit box without hardcoded*/
 		if(x>470 || x<320 || y<185 || y>400){ // Gotta move the mouse
 			System.out.println("Way outside the hit box");
 		}
-		else if(y<216 && y>189){ //New game area
+		else if(y<215 && y>180){ //New game area
 			//New Game method
 			System.out.println("New Game Selected");
 			f.dispose();
 			new Wolf3D();
 		}
-		else if(y<270 && y>244){ //Load Game area
+		else if(y<270 && y>245){ //Load Game area
 			//Loads previous game or opens file selector to pick the game
 			System.out.println("Load Game Selected");
 		}
-		else if(y<341 && y>312){ //Help area
+		else if(y<340 && y>310){ //Help area
 			//Loads help screen.
 			System.out.println("Help Selected");
 		}
-		else if(y<399 && y>373){ //Exit area
+		else if(y<400 && y>375){ //Exit area
 			System.out.println("Exit selected");
 			System.exit(0);
 		}
