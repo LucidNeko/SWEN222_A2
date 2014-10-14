aacce6103f577d1e4da44360d6de78036195b946
vanvljosh <vanvljosh@myvuw.ac.nz>
2014-10-15 01:24:32 +1300

    Created tests for Database classes

---
+++ Wolf3D/src/wolf3d/tests/DataManagementTests.java
@@ -0,0 +1,93 @@
+package wolf3d.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import wolf3d.EntityFactory;
+import wolf3d.components.Weight;
+import wolf3d.components.behaviours.PickUp;
+import wolf3d.components.sensors.ProximitySensor;
+import wolf3d.database.DataManagement;
+import wolf3d.world.Parser;
+
+import com.google.gson.Gson;
+
+import engine.components.Component;
+import engine.core.Entity;
+import engine.core.World;
+import engine.util.ServiceLocator;
+
+/**
+ * Test suite for the wolf3d.database DataManagement class.
+ *
+ * @author Joshua van Vliet
+ *
+ */
+public class DataManagementTests {
+	private World world;
+	private Component component;
+
+	@Before
+	public void initWorld(){
+		Entity item1, item2, item3, player;
+		//create world with player holding three items
+		World world = ServiceLocator.getService(World.class);
+		Parser parser = new Parser("map00/");
+
+		player = EntityFactory.createPlayer("Player",-1);
+		parser.createEntities(player);
+
+		item1 = world.createEntity("motorbike");
+		item1.attachComponent(ProximitySensor.class).setTarget(player);;
+		item1.attachComponent(new PickUp());
+		item1.attachComponent(new Weight(50));
+		item1.getComponent(PickUp.class).pickUpItem();
+
+		item2 = world.createEntity("bottle");
+		item2.attachComponent(ProximitySensor.class).setTarget(player);;
+		item2.attachComponent(new PickUp());
+		item2.attachComponent(Weight.class);
+		item2.getComponent(PickUp.class).pickUpItem();
+
+		item3 = world.createEntity("jar");
+		item3.attachComponent(ProximitySensor.class).setTarget(player);;
+		item3.attachComponent(new PickUp());
+		item3.attachComponent(Weight.class);
+		item3.getComponent(PickUp.class).pickUpItem();
+
+		this.world = world;
+	}
+
+
+	@Test
+	// Test component converts to Json without crashing
+	public void testCompToJson() {
+		component = new Component();
+		Gson gson = new Gson();
+		gson.toJson(component);
+	}
+
+	@Test
+	// Test class fromJson is correct for a component
+	public void testCompToJsonAndBack() {
+		component = new Component();
+		Gson gson = new Gson();
+		String json = gson.toJson(component);
+		assertTrue(gson.fromJson(json, Component.class) instanceof Component);
+	}
+
+	@Test
+	// Test a world saves without crashing.
+	public void testSaveWorldCrash() {
+		DataManagement.saveWorld("testSaveWorldCrash.txt", world);
+	}
+
+	@Test
+	// Test a world loads without crashing
+	public void testLoadWorld() {
+		DataManagement.loadWorld("testLoadWorldCrash.txt");
+	}
+
+}
---
+++ Wolf3D/src/wolf3d/tests/WorldBuilderTests.java
@@ -0,0 +1,80 @@
+package wolf3d.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import wolf3d.EntityFactory;
+import wolf3d.components.Health;
+import wolf3d.components.Inventory;
+import wolf3d.components.Strength;
+import wolf3d.components.Weight;
+import wolf3d.components.behaviours.PickUp;
+import wolf3d.components.sensors.ProximitySensor;
+import wolf3d.database.WorldBuilder;
+import wolf3d.world.Parser;
+import engine.components.Transform;
+import engine.core.Entity;
+import engine.core.World;
+import engine.util.ServiceLocator;
+
+/**
+ * Test suite for the wolf3d.database WorldBuilder class.
+ *
+ * @author Joshua van Vliet
+ *
+ */
+public class WorldBuilderTests {
+	private WorldBuilder builder;
+
+	@Test
+	//Test player creates without crashing.
+	public void testCreatePlayer() {
+		builder = new WorldBuilder("map00/");
+		//Create required parameters to call createPlayer
+		int uniqueID = (-2);
+		String name = "Player";
+		Transform transform = new Transform();
+		Health health = new Health();
+		Strength strength = new Strength();
+		Weight weight = new Weight();
+		Inventory inventory = new Inventory();
+		//Create player
+		builder.createPlayer(uniqueID, name, transform, health, strength, weight, inventory);
+	}
+
+	@Test
+	//Test camera creates without crashing, needs a player to be created first.
+	public void testCreateCamera() {
+		builder = new WorldBuilder("map00/");
+		//Create required parameters to call createPlayer
+		int uniqueID = (-3);
+		String name = "Player";
+		Transform transform = new Transform();
+		Health health = new Health();
+		Strength strength = new Strength();
+		Weight weight = new Weight();
+		Inventory inventory = new Inventory();
+		//Create player then camera
+		builder.createPlayer(uniqueID, name, transform, health, strength, weight, inventory);
+		builder.createCamera();
+	}
+
+	@Test
+	//Test default objects are created without crashing, needs a player to be created first.
+	public void testCreateDefaultObjects() {
+		builder = new WorldBuilder("map00/");
+		//Create required parameters to call createPlayer
+		int uniqueID = (-4);
+		String name = "Player";
+		Transform transform = new Transform();
+		Health health = new Health();
+		Strength strength = new Strength();
+		Weight weight = new Weight();
+		Inventory inventory = new Inventory();
+		//Create player then default objects
+		builder.createPlayer(uniqueID, name, transform, health, strength, weight, inventory);
+		builder.createDefaultObjects();
+	}
+}
