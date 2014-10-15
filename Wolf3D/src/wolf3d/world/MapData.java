db2d1e255d9f794e9c5c189a27dc5f93a70cc043
magansame <magansame@stout.ecs.vuw.ac.nz>
2014-10-15 16:45:08 +1300

    Added MapData to encapsulate walls and doors and can then be registered with the Service Locator

---
+++ Wolf3D/src/wolf3d/world/MapData.java
@@ -0,0 +1,49 @@
+package wolf3d.world;
+
+import engine.util.Service;
+
+public class MapData implements Service {
+
+	private Cell[][] walls;
+	private Cell[][] doors;
+
+	public MapData(Cell[][] walls, Cell[][] doors){
+		this.walls = walls;
+		this.doors = doors;
+	}
+
+	/**
+	 * Gets the wall for the given row and col
+	 * @param row the row to be referenced
+	 * @param col the col to be referenced
+	 * @return the int representing wall at the given position
+	 */
+	public int getWall(int row, int col){
+		return walls[row][col].getWalls();
+	}
+
+	/**
+	 * @return the width of the walls array
+	 */
+	public int getWallsWidth(){
+		return walls[0].length;
+	}
+
+	/**
+	 *
+	 * @return the height of the walls array
+	 */
+	public int getWallsHeight(){
+		return walls.length;
+	}
+
+	/**
+	 * Gets the door for the given row and col
+	 * @param row the row to be referenced
+	 * @param col the col to be referenced
+	 * @return the int representing door at the given position
+	 */
+	public int getDoors(int row, int col){
+		return doors[row][col].getWalls();
+	}
+}
